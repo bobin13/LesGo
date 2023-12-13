bb55cdbcd61b9803d10da5e8ed149964dda35e79
soniaks123 <95455498+soniaks123@users.noreply.github.com>
2023-12-13 13:49:00 -0500

    Update Ride.cs

--- Models/Ride.cs
+++ Models/Ride.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-
+//Author: Sasi Venkatesan
 namespace LesGo.Models
 {
     public class Ride
