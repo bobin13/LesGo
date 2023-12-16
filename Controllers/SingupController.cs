60b711f2cad2cd11c7885be13398d096c2efedb6
venkasas <122641014+venkasas@users.noreply.github.com>
2023-12-15 22:34:25 -0500

    SingupController.cs created

--- Controllers/SingupController.cs
+++ Controllers/SingupController.cs
@@ -1,1 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment2.Response;
+using LesGo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
+//Author: sasi 
+//# 991542294
+namespace LesGo.Controllers
+{
+
+    public class LoginRequest
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private DB _db = new DB();
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] User user)
+        {
+            if (user == null)
+                return BadRequest();
+
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            return Created("User Created", user);
+        }
+
+
+    }
+}
