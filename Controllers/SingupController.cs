7c94550af93f51f6c71939accf6f8fc775de733c
venkasas <122641014+venkasas@users.noreply.github.com>
2023-12-15 22:36:01 -0500

    Updated SingupController.cs

--- Controllers/SingupController.cs
+++ Controllers/SingupController.cs
@@ -8,20 +8,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 
-//Author: sasi 
+//Author: Sasidurka Venkatesan 
 //# 991542294
 namespace LesGo.Controllers
 {
 
-    public class LoginRequest
-    {
-        public string username { get; set; }
-        public string password { get; set; }
-    }
+
 
     [ApiController]
     [Route("api/[controller]")]
-    public class LoginController : ControllerBase
+    public class SingupController : ControllerBase
     {
         private DB _db = new DB();
 
@@ -34,7 +30,7 @@ namespace LesGo.Controllers
             await _db.Users.AddAsync(user);
             await _db.SaveChangesAsync();
 
-            return Created("User Created", user);
+            return Created("New User Created", user);
         }
 
 
